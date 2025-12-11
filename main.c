f67bee6db139514a04d7b526d8bb77a250ba4f9d
Ido Eitach <idoeitach98@gmail.com>
2025-12-11 14:00:53 +0200

    adding the structre of the project

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// #include "rawsock.h"
+// #include "forward.h"
+
+int main() {
+    const char *in_iface = "Ethernet";
+    const char *out_iface = "Ethernet";
+    
+    printf("hello world\n");
+    return 0;
+}
