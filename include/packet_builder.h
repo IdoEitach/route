20a35e9a9be7852602305ce0e8ab4481b21c7511
daniel <venom@gmail.com>
2025-12-28 10:01:42 +0200

    packet builder adding the udp test build

--- include/packet_builder.h
+++ include/packet_builder.h
@@ -1,21 +1,21 @@
 #ifndef PACKET_BUILDER_H
 #define PACKET_BUILDER_H
 
-#include <string>
-#include <vector>
+#include <arpa/inet.h>
 #include <cstdint>
 #include <cstring>
-#include <arpa/inet.h>
+#include <iostream>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
+#include <string>
+#include <vector>
 
+void print_packet(const std::vector<uint8_t> &packet);
 
-std::vector<uint8_t> build_udp_packet(
-    const std::string &src_ip,
-    uint16_t src_port,
-    const std::string &dest_ip,
-    uint16_t dest_port,
-    const std::string &payload
-);
+std::vector<uint8_t> build_udp_packet(const std::string &src_ip,
+                                      uint16_t src_port,
+                                      const std::string &dest_ip,
+                                      uint16_t dest_port,
+                                      const std::string &payload);
 
 #endif // PACKET_BUILDER_H
--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
 #include "hash_table.h"
+#include <iostream>
 
+std::unordered_map<uint16_t, std::string> ip_port_map;
 
-std::unordered_map< uint16_t, std::string > ip_port_map;
-
-void insert_ip_port( uint16_t port, const std::string &ip ) {
-    ip_port_map[port] = ip;
+void insert_ip_port(uint16_t port, const std::string &ip) {
+  ip_port_map[port] = ip;
 }
 
 void print_table() {
-    std::cout << "Port-IP Hash Table Contents:\n";
-    for (const auto &entry : ip_port_map) {
-        std::cout << " well Port: " << entry.first << " -> IP: " << entry.second << "\n";
-    }
+  std::cout << "Port-IP Hash Table Contents:\n";
+  for (const auto &entry : ip_port_map) {
+    std::cout << " Port: " << entry.first << " -> IP: " << entry.second << "\n";
+  }
 }
 
-void remove_ip_port( uint16_t port ) {
-    ip_port_map.erase(port);
-}
+void remove_ip_port(uint16_t port) { ip_port_map.erase(port); }
